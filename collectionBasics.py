befd0cb42600c52dc24f79a96a3002bb50a6f9c6
Deb <212393369@logon.ds.ge.com>
2018-11-23 19:18:04 +0530

    Initial commit

--- collectionBasics.py
+++ collectionBasics.py
@@ -1,3 +1,5 @@
+import copy
+
 # tuple is a heterogeneous immutable sequence of arbitrary objects; cannt be changed
 # can contain any object like nested tuple
 t = ("Norway",4.55,10)
@@ -41,8 +43,58 @@ print(b)
 print(5 in (3,5,17,88,9))
 print(5 not in (3,5,17,88,9))
 
-# Join/Split str
+# String operations
 values = ",".join(["One","Two","Three"])
-print(values)
-print(values.split(","))
-print("".join(["New","York"]))
+print(values)                               # join string seperated by ","
+print(values.split(","))                    # split string based on ","
+print("".join(["New","York"]))              # concatenate string using join
+print(len("check length"))                  # check length
+print("unforgetable".partition("forget"))   # partition based on a value
+pre,separator,post = "London:Britain".partition(":")
+print(pre)
+print(post)
+
+# List operations
+s ="show how to index into sequences".split()   # returns a list of string
+print(s)
+copy1 = s[:]            # copy list
+print(s is copy1)
+print(s == copy1)
+copy1[0]="1111"
+print(s)
+print(copy1)
+copy2 = s.copy()            # copy list
+print(copy2)
+copy2[0]="2222"
+print(s)
+print(copy2)
+
+copy3 = list(s)
+copy3[0]="3333"
+print(s)
+print(copy3)
+
+# shallow copy
+org = [[1,2,3],[4,5,6]]
+copy1 = copy.copy(org)
+copy1[0][1]=10
+print(org)
+print(copy1)
+
+# deep copy
+org = [[1,2,3],[4,5,6]]
+copy1 = copy.deepcopy(org)
+copy1[0][1]=10
+print(org)
+print(copy1)
+
+# list operations
+lst = "this is a python list object. Use this for testing".split()
+print(lst.index("python"))          # index of an element
+print(lst.count("this"))            # count of an element
+print("this" in lst)
+print("this" not in lst)
+lst.remove("this")                  # remove an element
+print(lst)
+lst.insert(0,"Hi!")
+print(lst)
